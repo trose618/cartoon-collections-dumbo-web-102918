c0ee54d1a3dce4a76d2af54c066d93ebea8f417b
Terrance Rose Jr <trose618@gmail.com>
2018-10-21 23:05:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,8 @@ def long_planeteer_calls(words)# code an argument here
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(foods)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
