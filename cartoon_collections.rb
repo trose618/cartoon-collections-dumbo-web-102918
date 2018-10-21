ca3751eaa43dd3d092b233b13ba24a660dc85336
Terrance Rose Jr <trose618@gmail.com>
2018-10-21 23:09:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,6 +23,5 @@ end
 def find_the_cheese(foods)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  foods.each do |food|
-    food.include?()
+  
 end
